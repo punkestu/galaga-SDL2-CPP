acf96418d0811db32b6409315f141fea240b6d39
punkestu <pangestubima89@gmail.com>
2021-01-28 21:54:35 +0700

    menambahkan mekanisme dasar pada class enemies

--- main.cpp
+++ main.cpp
@@ -11,13 +11,13 @@ private:
     SDL_Rect body[2];
     std::vector<vect2d> bullets;
     bool left,right,shoot,skey;
-    int start;
+    Uint32 start;
 public:
     player(){
         body[0] = {210, 440, 60, 20};
         body[1] = {230, 420, 20, 20};
         left = right = shoot = skey = 0;
-        start = 999999;
+        start = 99999999;
     }
 
     void control(SDL_Event* event){
@@ -96,6 +96,63 @@ public:
 
 class enemy{
 private:
+    SDL_Rect body;
+    std::vector<vect2d> bullets;
+    bool left,right,shoot;
+    int start, moving, movHor;
+public:
+    enemy(vect2d startPos, int moveD){
+        body.x = startPos.x;
+        body.y = startPos.y;
+        body.w = 60;
+        body.h = 60;
+        left = right = shoot = 0;
+        if(moveD == 1){
+            right = 1;
+        }else{
+            left = 1;
+        }
+        start = SDL_GetTicks();
+        moving = SDL_GetTicks();
+        movHor = SDL_GetTicks();
+    }
+
+    void moveF(){
+        body.y+=70;
+    }
+
+    void update(){
+        if(SDL_GetTicks()-moving >= 10*1000){
+            moveF();
+            moving = SDL_GetTicks();
+        }
+        if(SDL_GetTicks()-movHor >= 250){
+            SDL_Log("move");
+            if(right){
+                body.x+=10;
+                if(body.x+body.h >= 470){
+                    right = 0;
+                    left = 1;
+                }
+            }
+            if(left){
+                body.x-=10;
+                if(body.x <= 10){
+                    right = 1;
+                    left = 0;
+                }
+            }
+            movHor = SDL_GetTicks();
+        }
+    }
+
+    void render(SDL_Renderer* renderer){
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &body);
+    }
+
+    std::vector<vect2d>* getBullets(){return &bullets;}
+    SDL_Rect getBody(){return body;}
 
 };
 
@@ -105,6 +162,8 @@ int main(int argc, char* argv[])
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
     player mplayer = player();
+    std::vector<enemy*> enemies;
+    enemies.push_back(new enemy({{10,10},1}));
 
     SDL_Event event;
     int start = SDL_GetTicks();
@@ -121,6 +180,9 @@ int main(int argc, char* argv[])
         //update
         if(SDL_GetTicks()-start>=1000/60){
             mplayer.update();
+            for(unsigned int i = 0; i < enemies.size(); i++){
+                enemies[i]->update();
+            }
             start = SDL_GetTicks();
         }
         //
@@ -130,19 +192,39 @@ int main(int argc, char* argv[])
         SDL_RenderClear(renderer);
 
         mplayer.render(renderer);
+        for(unsigned int i = 0; i < enemies.size(); i++){
+            enemies[i]->render(renderer);
+        }
         //
 
         unsigned int i = 0;
         for(std::vector<vect2d>::iterator it = mplayer.getBullets()->begin(); i < mplayer.getBullets()->size() ; it++){
-            if(it->y<=20){
+            if(it->y<=10){
                 mplayer.getBullets()->erase(it);
                 it--;
                 i--;
+            }
+            for(unsigned int i = 0; i < enemies.size(); i++){
+                if(it->x<=enemies[i]->getBody().x+60&&it->x+10>=enemies[i]->getBody().x){
+                    if(it->y<=enemies[i]->getBody().y+60){
+                        delete enemies[i];
+                        enemies.clear();
+                        mplayer.getBullets()->erase(it);
+                        it--;
+                        i--;
+                        break;
+                    }
+                }
             }
             i++;
         }
 
         SDL_RenderPresent(renderer);
+    }
+
+    for(unsigned int i = 0; i < enemies.size(); i++){
+        delete enemies[i];
+        enemies.clear();
     }
 
     SDL_DestroyRenderer(renderer);
